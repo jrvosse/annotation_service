1d3dfb8e96a6a102bea963f52b8b7b3c2bd1e7bb
Jacco van Ossenbruggen <Jacco.van.Ossenbruggen@cwi.nl>
2012-06-08 11:05:33 +0200

    oops. forgot module decl

--- config-available/annotation_service.pl
+++ config-available/annotation_service.pl
@@ -3,6 +3,7 @@
 /** <module> Configurable RDF annotation webservice
 */
 
+:- use_module(library(semweb/rdf_db)).
 :- rdf_register_ns(oac, 'http://www.openannotation.org/ns/').
 :- rdf_register_ns(an, 'http://semanticweb.cs.vu.nl/annotate/').
 
