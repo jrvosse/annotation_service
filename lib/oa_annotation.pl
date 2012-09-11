a796f30b68d98a227e87d6330efd08f46579b6c9
Jacco van Ossenbruggen <Jacco.van.Ossenbruggen@cwi.nl>
2012-09-11 14:33:53 +0200

    added get annotation

--- lib/oa_annotation.pl
+++ lib/oa_annotation.pl
@@ -1,12 +1,22 @@
 :- module(oa_annotation, [
+			  rdf_has_graph/4,
 			  rdf_add_annotation/2,
+			  rdf_get_annotation/3,
 			  rdf_remove_annotation/2
 			 ]).
 
 :- use_module(library(semweb/rdf_db)).
+:- use_module(library(semweb/rdf_label)).
 :- use_module(library(graph_version)).
 :- use_module(library(oa_schema)).
 
+:- rdf_meta
+	rdf_has_graph(r,r,r,r).
+
+rdf_has_graph(S,P,O,G) :-
+	rdf_graph(G),
+	rdf_has(S,P,O,RP),
+	rdf(S,RP,O,G).
 
 rdf_add_annotation(Options, Annotation) :-
 	option(comment(Comment), Options),
@@ -49,19 +59,42 @@ rdf_add_annotation(Options, Annotation) :-
 
 po2rdf(S,po(P,O),rdf(S,P,O)).
 
-%%	rdf_get_annotation(+Annotation, +Graph, -PropList) is det.
+%%	rdf_get_annotation(+Annotation, +Graph, -Props) is nondet.
 %
-%	To be implemented later.
-
-rdf_get_annotation(+Annotation, +Graph, Props) :-
+%	Props is an option list with the properties of Annotation
+%	in Graph.
+%
+%	Hack:- You can filter on annotationField(F), user(U) by putting 
+%	these in the Props as the first two properties...
+rdf_get_annotation(Target, Graph, Props) :-
 	rdf(Annotation, oa:hasTarget, Target, Graph),
-	Props =
-	[ target(Target)
+	rdf(Annotation, an:annotationField, Field, Graph),
+	rdf_has_graph(Annotation, oa:annotator, User, Graph),
+	rdf_has_graph(Annotation, oa:hasBody, Body, Graph),
+	rdf_has_graph(Annotation, dcterms:title, Lit, Graph),
+	(   rdf_has_graph(Annotation, rdfs:comment, Comment0, Graph)
+	->  literal_text(Comment0, Comment)
+	;   Comment=''
+	),
+	(   rdf_has_graph(Annotation, an:unsure, Unsure0, Graph)
+	->  literal_text(Unsure0, Unsure)
+	;   Unsure=''
+	),
+	literal_text(Lit, Label),
+
+	Props = [
+		 annotationField(Field),
+		 user(User),
+		 annotation(Annotation),
+		 comment(Comment),
+		 unsure(Unsure),
+		 label(Label),
+		 body(Body)
 	].
 
 %%	rdf_remove_annotation(+Annotation, ?Target) is det.
 %
-%	Removes Annotation on Target. Succeeds also if Annotation
+%	Removes Annotation on Target. Also succeeds if Annotation
 %	does not exists.
 
 rdf_remove_annotation(Annotation, Target) :-
